6e8b0e6f7856231bf69a54a7a0bf1be70298221e
Subas Tharu <kstrings346@gmail.com>
2026-02-15 13:49:39 +0545

    made login and signup page dynamic

--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -2,10 +2,48 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+
+
+
+#[Title('Register')]
 
 class RegisterPage extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+
+    //Register User
+
+    public function save(){
+        $this->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|min:6|max:255',
+        ]);
+
+        //save to database
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+
+        //login user
+        Auth::login($user);
+
+        //redirect to home page
+
+        return redirect()->intended('/');
+
+    }
+
+
     public function render()
     {
         return view('livewire.auth.register-page');
--- routes/web.php
+++ routes/web.php
@@ -15,25 +15,32 @@ use App\Livewire\ProductDetailpage;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Auth\ResetPasswordPage;
 use App\Livewire\Auth\ForgotPasswordPage;
-
-Route::get('/',HomePage::class);
-
-Route::get('/categories',CategoriesPage::class);
-Route::get('/products',ProductsPage::class);
-Route::get('/cart',CartPage::class);
-Route::get('/products/{slug}',ProductDetailpage::class);
+use Illuminate\Support\Facades\Auth;
 
 
-Route::get('/checkout',CheckoutPage::class);
-Route::get('/my-orders',MyOrdersPage::class);
-Route::get('/my-orders/{order}', MyOrderDetailPage::class);
+Route::get('/', HomePage::class);
+
+Route::get('/categories', CategoriesPage::class);
+Route::get('/products', ProductsPage::class);
+Route::get('/cart', CartPage::class);
+Route::get('/products/{slug}', ProductDetailpage::class);
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', LoginPage::class);
+    Route::get('/register', RegisterPage::class);
+    Route::get('/forgot', ForgotPasswordPage::class);
+    Route::get('/reset', ResetPasswordPage::class);
+});
 
 
-
-Route::get('/login',LoginPage::class);
-Route::get('/register',RegisterPage::class);
-Route::get('/forgot',ForgotPasswordPage::class);
-Route::get('/reset',ResetPasswordPage::class);
-
-Route::get('/success',SuccessPage::class);
-Route::get('/cancel',CancelPage::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/logout', function () {
+        Auth::logout();
+        return redirect('/');
+    });
+    Route::get('/checkout', CheckoutPage::class);
+    Route::get('/my-orders', MyOrdersPage::class);
+    Route::get('/my-orders/{order}', MyOrderDetailPage::class);
+    Route::get('/success', SuccessPage::class);
+    Route::get('/cancel', CancelPage::class);
+});
